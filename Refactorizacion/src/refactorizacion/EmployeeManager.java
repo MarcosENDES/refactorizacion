6ea5352a3c840a91bafa6ae8226716213895c613
OEM Configuration (temporary user) <rios.garcia.marcos@iescamas.es>
2025-02-06 10:16:23 +0100

    Cambios sin terminar

--- Refactorizacion/src/refactorizacion/EmployeeManager.java
+++ Refactorizacion/src/refactorizacion/EmployeeManager.java
@@ -23,10 +23,4 @@ public class EmployeeManager {
         }
     }
     
-    public static void main(String[] args) {
-        EmployeeManager manager = new EmployeeManager();
-        manager.addEmployee("John Doe", 5);
-        manager.addEmployee("Jane Smith", 2);
-        manager.printEmployees();
-    }
 }
